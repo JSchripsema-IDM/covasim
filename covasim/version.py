0896c2ede19c8daecb82bd40c14b3634ef9d848d
Jen Schripsema <jschripsema@idmod.org>
2020-04-16 17:54:10 -0600

    initial commit of built HTML docs for Covasim

--- covasim/version.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__all__ = ['__version__', '__versiondate__', '__license__']
-
-__version__ = '0.27.1'
-__versiondate__ = '2020-04-15'
-__license__ = f'Covasim {__version__} ({__versiondate__}) — © 2020 by IDM'
